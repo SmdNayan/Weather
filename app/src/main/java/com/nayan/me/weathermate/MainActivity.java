5658c0a86878615fd7675c26f609dd3a88f97054
SmdNayan <smd.nayan69@gmail.com>
2017-04-18 14:01:40 +0600

    firt commit

---
+++ app/src/main/java/com/nayan/me/weathermate/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nayan.me.weathermate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
