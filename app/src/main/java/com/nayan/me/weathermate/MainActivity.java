4d566af02361f9256ac02ec3bd4a4c4f6c94964a
SmdNayan <smd.nayan69@gmail.com>
2017-04-20 02:27:25 +0600

    update menu

--- app/src/main/java/com/nayan/me/weathermate/MainActivity.java
+++ app/src/main/java/com/nayan/me/weathermate/MainActivity.java
@@ -1,7 +1,14 @@
 package com.nayan.me.weathermate;
 
+import android.content.ClipData;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import static android.os.Build.VERSION_CODES.M;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case (R.id.farenhite):
+                Toast.makeText(this, "Temparature in farenhite", Toast.LENGTH_SHORT).show();
+                break;
+            case (R.id.home):
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
